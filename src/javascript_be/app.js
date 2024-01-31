561773c116f8c13296eb26184fa688b13077f621
Eugeniusz <58223087+legacyDriven@users.noreply.github.com>
2024-01-31 01:34:39 +0100

    fourth commit, javascript backend updated

--- src/javascript_be/app.js
+++ src/javascript_be/app.js
@@ -30,14 +30,27 @@ app.post('/calculate-bmi', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    const { email } = req.body;
-    // Regex dla walidacji e-maila zgodny ze standardem RFC
+    const { email, firstName, lastName, pesel } = req.body;
+    // Walidacja e-maila
     const emailRegex = /^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$/;
     if (!emailRegex.test(email)) {
         return res.send('Podano nieprawidłowy adres e-mail.');
     }
 
-    res.send('Rejestracja zakończona sukcesem. Twój e-mail: ' + email);
+    // Walidacja imienia i nazwiska
+    if (firstName.length < 3 || firstName.length > 20 || lastName.length < 3 || lastName.length > 20) {
+        return res.send('Imię lub nazwisko ma nieprawidłową długość.');
+    }
+
+    // Walidacja PESEL
+    const peselRegex = /^\d{11}$/;
+    if (!peselRegex.test(pesel)) {
+        return res.send('Podano nieprawidłowy PESEL.');
+    }
+
+    // Tutaj można dodać bardziej skomplikowaną logikę walidacji PESEL (np. sprawdzanie daty urodzenia i cyfry kontrolnej)
+
+    res.send('Rejestracja zakończona sukcesem. Dane: ' + JSON.stringify({ email, firstName, lastName, pesel }));
 });
 
 app.listen(port, () => {
